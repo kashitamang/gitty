c257cf39e2c8dbfbf06e016a8bed3f97c4e2d96a
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-10 14:40:11 -0700

    adding cross-fetch npm install to package lock, adds get callback and get dash to index, adding github.js to mock folder for testing. passes test should login and redirect user to dashboard

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+//mock thing
 jest.mock('../lib/services/github');
 
 describe('backend-express-template routes', () => {
