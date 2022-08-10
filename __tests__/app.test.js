e9193680549c187020e4f8e43ef168fc8466feff
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-10 12:13:04 -0700

    should login and redirect users to /api/v1/github/dashboard

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,33 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/github');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+
   afterAll(() => {
     pool.end();
   });
+
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+
 });
