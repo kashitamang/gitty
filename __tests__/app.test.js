68db28afde7adf22320bf6f663925b9d0ccc55ee
KASHI TAMANG <tamang.kashi@gmail.com>
2022-08-10 19:51:47 -0700

    moving post tests into separate testing file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,18 +31,4 @@ describe('backend-express-template routes', () => {
     });
     // console.log('res.body', res.body);
   });
-
-  it('#GET /posts, authenticated users can see a list of posts at /api/v1/posts', async () => {
-    const agent = request.agent(app);
-    await agent.get('/api/v1/github/callback?code=42').redirects(1);
-    const res = await agent.get('/api/v1/posts');
-    console.log('response body', res.body);
-
-    expect(res.status).toBe(200);
-
-    expect(res.body[0]).toEqual({
-      content: expect.any(String),
-      created_at: expect.any(String),
-    });
-  });
 });
---
+++ __tests__/posts.test.js
@@ -0,0 +1,31 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+//mock user
+jest.mock('../lib/services/github');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('#GET /posts, authenticated users can see a list of posts at /api/v1/posts', async () => {
+    const agent = request.agent(app);
+    await agent.get('/api/v1/github/callback?code=42').redirects(1);
+    const res = await agent.get('/api/v1/posts');
+    console.log('response body', res.body);
+
+    expect(res.status).toBe(200);
+
+    expect(res.body[0]).toEqual({
+      content: expect.any(String),
+      created_at: expect.any(String),
+    });
+  });
+});
